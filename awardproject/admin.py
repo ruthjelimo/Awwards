efc177fd2f3f7d87ee3148880290cae57440cd01
ruthmimo <ruthjmimo@gmail.com>
2022-04-08 22:36:19 +0300

    updated file

--- awardproject/admin.py
+++ awardproject/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Project, Rating
+
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rating)
